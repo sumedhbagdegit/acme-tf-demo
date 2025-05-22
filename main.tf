79ccd9f09d3854d3c8ec6426c8e9103d69e3fff0
Sumedh Bagde <sumedh.bagde@hashicorp.com>
2025-05-22 17:00:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,13 +8,13 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "web" {
   ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-}
+
   tags = {
     Name = "WebServer"
     Env  = "Production"
