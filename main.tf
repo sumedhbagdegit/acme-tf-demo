1622ec18999dd46af39cd3ded67d6ac30cf7f996
Sumedh Bagde <sumedh.bagde@hashicorp.com>
2025-05-22 16:52:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,8 @@ provider "aws" {
 
 resource "aws_instance" "web" {
   ami           = "ami-0c7217cdde317cfec" # Ubuntu 22.04 LTS
-  instance_type = "m5.large" # Expensive instance  tags = {
+  instance_type = var.instance_type
+  tags = {
     Name = "ACME-WebServer"
   }
 }
