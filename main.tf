28cee12be6d5539225f3c428a3a8ab4c86347185
Sumedh Bagde <sumedh.bagde@hashicorp.com>
2025-05-22 16:11:55 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c7217cdde317cfec" # Ubuntu 22.04 LTS
+  instance_type = var.instance_type
+  tags = {
+    Name = "ACME-WebServer"
+  }
+}
